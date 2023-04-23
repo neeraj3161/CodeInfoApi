cb0a0a3cc86f80580d10cbcb2aabc30afd983646
neeraj3161 <neerajtripathi7447@gmail.com>
2023-04-23 16:54:59 +0530

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace CodeInfoApi.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
